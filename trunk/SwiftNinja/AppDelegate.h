2fb2c65cd59c8395ecf865a5594507ed81c2cb2b
veselin.vl.ivanov@gmail.com <veselin.vl.ivanov@gmail.com@b90388f0-ada5-ad64-9721-9c261a3eca2c>
2015-02-23 20:35:56 +0000

    First commit

---
+++ trunk/SwiftNinja/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwiftNinja
+//
+//  Created by game on 2/23/15.
+//  Copyright (c) 2015 VP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ trunk/SwiftNinja/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SwiftNinja
+//
+//  Created by game on 2/23/15.
+//  Copyright (c) 2015 VP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
